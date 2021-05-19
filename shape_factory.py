a6d5725e5828f1c97b067216b20d1ebf54afcf40
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2021-05-20 00:32:13 +0200

    refactor: resolve some pylint warnings and disable R0903 warning

--- shape_factory.py
+++ shape_factory.py
@@ -1,28 +1,34 @@
 '''Implement a Factory (design pattern) about shapes (circle, square, rectangle).'''
 
+# pylint: disable=R0903
+
 class Shape():
     '''Shape interface.'''
-    def draw(self, shape, emoji) -> None:
+    @staticmethod
+    def draw(shape_type, emoji) -> None:
         '''Specific shape and custom emoji should be provided by child classes.'''
-        print(f'\nA {shape} {emoji} has been drawn', end='')
+        print(f'\nA {shape_type} {emoji} has been drawn', end='')
 
 class Circle(Shape):
     '''Circle class.'''
-    def draw(self) -> None:
-        super().draw('circle', '🔵')
+    def draw(self, shape_type='circle', emoji='🔵') -> None:
+        super().draw(shape_type, emoji)
 
 class Square(Shape):
     '''Square class.'''
-    def draw(self) -> None:
-        super().draw('square', '🟧')
+    def draw(self, shape_type='square', emoji='🟧') -> None:
+        super().draw(shape_type, emoji)
 
 class Rectangle(Shape):
     '''Rectangle class.'''
-    def draw(self) -> None:
-        super().draw('rectangle', '💳')
+    def draw(self, shape_type='rectangle', emoji='💳') -> None:
+        super().draw(shape_type, emoji)
 
 class ShapeFactory():
+    '''Shape factory.'''
+    @staticmethod
     def get_shape(shape_type: str) -> Shape:
+        '''Instantiate a different class regarding the shape type.'''
         if shape_type == 'circle':
             return Circle()
         if shape_type == 'square':
